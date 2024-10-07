0a3ffa9bc58eb21c3190620098e4dcef4cb7f537
myusername <farhadhossen2590@gmail.com>
2024-10-08 00:36:36 +0600

    Moderator Select api Added

--- routes/cofirmEventRoute.js
+++ routes/cofirmEventRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getConfirmEvent,getEmailConfirmEvent,postConfirmEvent } = require('../controller/confirmEventCollection');
+const { getConfirmEvent,getEmailConfirmEvent,postConfirmEvent,updateEventData } = require('../controller/confirmEventCollection');
 
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/eventConfirmed', getConfirmEvent)
 router.get('/confirmEvents/:email', getEmailConfirmEvent)
 router.post('/confirmEvents', postConfirmEvent)
+router.put('/addOrganizer/:id',updateEventData)
 
 module.exports = router;
