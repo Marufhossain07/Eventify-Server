c87e4c4158303e218bb8b20ae17ce2e260df94de
mim chowdhury <umac3333@gmail.com>
2024-10-09 19:17:13 +0600

    manage-user-added

--- controller/userCollection.js
+++ controller/userCollection.js
@@ -1,5 +1,5 @@
 const { getDatabase } = require("../config/db")
-
+const { ObjectId } = require("mongodb")
 
 const getUser = async (req, res) => {
     const db = getDatabase()
@@ -27,34 +27,88 @@ const postUser = async (req, res) => {
     res.send(addUser);
 };
 
-const getAdmin = async(req,res)=>{
+const getAdmin = async (req, res) => {
     const db = getDatabase()
     const userCollections = db.collection('users')
 
     const email = req.params.email;
-    const query = {email: email};
+    const query = { email: email };
     const user = await userCollections.findOne(query)
 
     let admin = false;
-    if (user){
+    if (user) {
         admin = user?.role === 'admin'
     }
-    res.send({admin})
+    res.send({ admin })
 }
-const getModerator = async(req,res)=>{
+const getModerator = async (req, res) => {
     const db = getDatabase()
     const userCollections = db.collection('users')
 
     const email = req.params.email;
-    const query = {email: email};
+    const query = { email: email };
     const user = await userCollections.findOne(query)
 
     let moderator = false;
-    if (user){
+    if (user) {
         moderator = user?.role === 'moderator'
     }
-    res.send({moderator})
+    res.send({ moderator })
 }
+
+
+
+
+
+// make admin api
+const makeAdmin = async (req, res) => {
+    const db = getDatabase()
+    const userCollections = db.collection('users')
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const updateDoc = {
+        $set: {
+            role: 'admin',
+
+        }
+    }
+    const result = await userCollections.updateOne(query, updateDoc);
+    res.send(result)
+
+}
+// make moderator api
+const makeModerator = async (req, res) => {
+    const db = getDatabase()
+    const userCollections = db.collection('users')
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const updateDoc = {
+        $set: {
+            role: 'moderator',
+
+        }
+    }
+    const result = await userCollections.updateOne(query, updateDoc);
+    res.send(result)
+
+}
+
+// app.delete('/wishlistDelete/:id', 
+// delete user
+const deleteUser = async (req, res) => {
+    const db = getDatabase()
+    const userCollections = db.collection('users')
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    console.log(query)
+    const result = await userCollections.deleteOne(query);
+    console.log(result)
+    res.send(result)
+}
+
+
+
+
 
 
 
@@ -62,5 +116,8 @@ module.exports = {
     getUser,
     postUser,
     getAdmin,
-    getModerator
+    getModerator,
+    makeAdmin,
+    makeModerator,
+    deleteUser
 };
