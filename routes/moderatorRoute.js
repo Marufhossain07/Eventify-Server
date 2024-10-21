8e79fc634899d88209916c2cb984e824a699ce89
myusername <farhadhossen2590@gmail.com>
2024-10-21 17:59:46 +0600

    New Updates

--- routes/moderatorRoute.js
+++ routes/moderatorRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { postRequiredItems,updateRequiredItems } = require('../controller/moderatorItems');
+const { postRequiredItems,updateRequiredItems,getItems } = require('../controller/moderatorItems');
 
 
 const router = express.Router();
 
+router.get('/items', getItems)
 router.post('/requiredItems', postRequiredItems)
 router.put('/requiredItem/:selectedEvent', updateRequiredItems);
 
